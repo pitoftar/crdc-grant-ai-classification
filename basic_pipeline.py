f77967f60860ddf7590471c5cbd368568b88a55f
Adrien Savard-Arseneault <savarseno@gmail.com>
2026-07-30 17:22:21 -0400

    dataframe en vrac avec les resultats de la classification par division

--- basic_pipeline.py
+++ basic_pipeline.py
@@ -18,36 +18,52 @@ classes = codes_uniques(df, 'code_c', 'class')
 sous_classes = codes_uniques(df, 'code_sc', 'subclass')
 
 # donnees projets
+# initialiser liste des titres seuls
+# initialiser liste des titres avec comites entre parenthese
+# (le cas echeant)
 dtfrm = pd.read_csv("data/smaller_sample.csv", sep=';', names=['comite_en', 'comite_fr', 'titre'])
 
 projets = list(dtfrm.T.to_dict().values())
 k = 'titre'
 c = 'comite_en'
+
 titres = [projet.get(k) for projet in projets if k in projet]
 
 titres_comites = []
+
 for projet in projets:
     if projet[c] is None:
         titres_comites.append(projet[k])
     else:
         titres_comites.append(f'{projet[k]} ({projet[c]})')
 
+# classification des projets selon la division
 
-# resultats = []
+resultats = []
 
-# for i, titre in enumerate(titres):
-#     resultat = classifier(titre, list(divisions.values()), multi_label=False) # multilabel false pour la classification au niveau de la division
-#     resultats.append(resultat)
-#     print(f"Grant #{i+1} DONE")
+for i, titre in enumerate(titres_comites):
+    resultat = classifier(titre, list(divisions.values()), multi_label=False) # multilabel false pour la classification au niveau de la division
+    resultats.append(resultat)
+    print(f"Grant #{i+1} DONE")
 
+output_net = []
 
-# for resultat in resultats:
-#     for k, v in resultat.items():
-#         if isinstance(v, list):
-#             for idx, val in enumerate(v, start=1):
-#                 print(f"{k} {idx} : {val}")
-#         else:
-#             print(f"{k} : {v}")
+for resultat in resultats:
+    rangee = {}
+    for k, v in resultat.items():
+        if isinstance(v, list):
+            for idx, val in enumerate(v, start=1):
+                rangee.update({f"{k} {idx}": val})
+        else:
+            rangee.update({k : v})
+    output_net.append(rangee)
+
+with open('out/dump.txt', 'w') as f:
+    f.write(str(output_net))
+    f.close()
+
+classification_division = pd.DataFrame(output_net)
+print(classification_division)
 
 # dump = pd.DataFrame.from_dict(resultats)
 # dump.to_csv('out/facebook-bart-large-mnli.csv', sep=';', mode='w', quotechar='"') # ajuster le titre en fonction du traitement de la classification
