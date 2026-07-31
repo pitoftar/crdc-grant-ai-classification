b80a0b88d357594c7fd77af718f5dda12893d3d1
Adrien Savard-Arseneault <savarseno@gmail.com>
2026-07-31 12:51:55 -0400

    classification preliminaire en francais, raffinement nomenclature output

--- classification/basic_pipeline.py
+++ classification/basic_pipeline.py
@@ -123,6 +123,7 @@ if not os.path.exists(f"../out/{re.sub('/', '-', MODEL)}/"):
 
 SEQ = 'tc' # tc pour titre et comité, t pour titre seulement
 FINE_TUNING = 'raw' # raw sans fine-tuning, finet avec fine-tuning
+LEVEL = 'div' # div pour division, gr pour groupe, divgr pour groupe d'apres division
 SCOPE = scope_map[DATASET]
 
-classification_division.to_csv(f"../out/{re.sub('/', '-', MODEL)}/{now}_{SEQ}_{FINE_TUNING}_{SCOPE}.csv", sep=';', mode='w', quotechar='"')
+classification_division.to_csv(f"../out/{re.sub('/', '-', MODEL)}/{now}_{SEQ}_{FINE_TUNING}_{LEVEL}_{SCOPE}.csv", sep=';', mode='w', quotechar='"')
