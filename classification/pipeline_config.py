9350ed05a01753e5622f07518c2fc9d8596a1343
Adrien Savard-Arseneault <savarseno@gmail.com>
2026-08-27 16:20:55 -0400

    nouvelles sorties

--- classification/pipeline_config.py
+++ classification/pipeline_config.py
@@ -4,4 +4,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_DIR = BASE_DIR / 'data'
 OUT_DIR = BASE_DIR / 'out'
-MODEL = 'MoritzLaurer/bge-m3-zeroshot-v2.0'
+MODEL = 'MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7'
