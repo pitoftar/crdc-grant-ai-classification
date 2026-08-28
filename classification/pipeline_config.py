944e3c3dc8a41d469628f1f6266aabd5fd88d219
Adrien Savard-Arseneault <savarseno@gmail.com>
2026-08-27 20:31:37 -0400

    sorties

--- classification/pipeline_config.py
+++ classification/pipeline_config.py
@@ -4,4 +4,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_DIR = BASE_DIR / 'data'
 OUT_DIR = BASE_DIR / 'out'
-MODEL = 'MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7'
+MODEL = 'facebook/bart-large-mnli'
